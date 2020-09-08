0abfd2b2d382be620805fb845afd0c601fd96200
fedeghe <fedeghe@gmail.com>
2020-09-08 08:04:07 +0200

    better singleton

--- creational/singleton.js
+++ creational/singleton.js
@@ -1,37 +1,38 @@
 var God = (function () {
+
+    God._instance = null;
+    // static instance attached to the following
+    
 	function God() {
 		this.power = Infinity;
 
-		//static !!!!
+		// check the static !!!!
 		if (God._instance) {
-			console.log('You will have only one God');
+            // already have it
 			return God._instance;
-		}
-		console.log('God has been created... humm....ok God created himself!');
+        }
+
+        // created, autoreturn being called as constructor, assign to local instance
 		God._instance = this;
 	}
 
-	// traditional proto
-	God.prototype.readHisThoughts = function () {
-		console.log('Thinking about undoing mankind!');
-	}
-
-	// static function !!!
+	// dummy proto method
+	God.prototype.doTheRightThingAsap = function () { console.log('Kill all humans!'); };
+	
+	// optional static function !!!
 	God.getInstance = function () {
 		if (!God._instance)
 			God._instance = new God();
 		return God._instance;
-	}
-	God._instance = null;
-
+    }
+    
 	return God;
 })();
 
-
+// =======================================================================================
+// USE IT
 
 var God1 = new God(),
 	God2 = new God();
-
-God1.readHisThoughts();
-
-console.log('Is there only one God? (..in You I mean): ' + (God1 === God2));
+God1.doTheRightThingAsap();
+console.log('One God: ' + (God1 === God2));
