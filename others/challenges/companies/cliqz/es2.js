6faf995cdfab464ffa08416dc616e284ded775eb
fedeghe <fedeghe@gmail.com>
2020-09-26 16:16:12 +0200

    wtf

--- others/challenges/companies/cliqz/es2.js
+++ others/challenges/companies/cliqz/es2.js
@@ -6,7 +6,7 @@ restituire il numero ordinato di coppie in A che sommate fanno K
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
 
-function solution(K, A) {
+function solution1(K, A) {
     var res = 0,
         map = {};
         
@@ -24,9 +24,37 @@ function solution(K, A) {
     A.forEach(function (el) {
         if (el in map) res += map[el];
     });
+     // slower functional
+    // A.filter(el => el in map).forEach(e => res+=map[e])
+
+    return res;
+}
+
+function solution2(K, A) {
+    var res = 0,
+        map = {},
+        i = 0, l = A.length;
+        
+    // create a map complementars -> occurrence
+    //
+    for (null; i < l; i++) {
+        var k = K - A[i];
+        if (!(k in map)) map[k] = 0;
+        map[k]++;
+    }
+
+    for (i = 0; i < l; i++) {
+        if (A[i] in map) res += map[A[i]];
+    }
     
     return res;
 }
 
+// both are O(n) with 
+// c1 < c2
 
-module.exports = solution;
+
+
+
+exports.solution1 = solution1;
+exports.solution2 = solution2;
--- others/challenges/companies/cliqz/test/test2.js
+++ others/challenges/companies/cliqz/test/test2.js
@@ -2,24 +2,47 @@ var assert = require('assert'),
 	s = require('../es2');
 
 describe('ex. 2', function() {
-	describe('#solution()', function() {
+	describe('#solution1()', function() {
 		it('no solutions', function() {
-			assert.equal(0, s(10, [1,1,1,1,1,1]));
-			assert.equal(0, s(3, [1,1,1,1,1,1]));
-			assert.equal(0, s(4, [4,4,4,4,4,4,5,6,7,8]));
-			assert.equal(0, s(4, [4,-1,4,1,4,-1,-4,1,4,1]));
-			assert.equal(0, s(1, [2,0,0,0,0,0,0,0,0,0]));
-			assert.equal(0, s(4, [1,0,0,1,0,0,1,0,1,0]));
+			assert.equal(0, s.solution1(10, [1,1,1,1,1,1]));
+			assert.equal(0, s.solution1(3, [1,1,1,1,1,1]));
+			assert.equal(0, s.solution1(4, [4,4,4,4,4,4,5,6,7,8]));
+			assert.equal(0, s.solution1(4, [4,-1,4,1,4,-1,-4,1,4,1]));
+			assert.equal(0, s.solution1(1, [2,0,0,0,0,0,0,0,0,0]));
+			assert.equal(0, s.solution1(4, [1,0,0,1,0,0,1,0,1,0]));
 		});
 		it('right # of solutions', function() {
-			assert.equal(20, s(0, [4,-1,4,1,4,-1,-4,1,4,1]));
-			assert.equal(1, s(2, [1,0,0,0,0,0,0,0,0,0]));
-			assert.equal(2, s(10, [2,1,1,1,8,1]));
-			assert.equal(6, s(10, [2, 8, 1, 9, 8]));
-			assert.equal(8, s(10, [2, 3, 7, 9, 2, 1, 8]));
-			assert.equal(7, s(10, [2, 8, 1, 9, 8, 5]));
-			assert.equal(2, s(11, [2, 8, 1, 9, 8, 5]));
-			assert.equal(4, s(13, [2, 8, 1, 9, 8, 5]));
+			assert.equal(20, s.solution1(0, [4,-1,4,1,4,-1,-4,1,4,1]));
+			assert.equal(1, s.solution1(2, [1,0,0,0,0,0,0,0,0,0]));
+			assert.equal(2, s.solution1(10, [2,1,1,1,8,1]));
+			assert.equal(6, s.solution1(10, [2, 8, 1, 9, 8]));
+			assert.equal(8, s.solution1(10, [2, 3, 7, 9, 2, 1, 8]));
+			assert.equal(7, s.solution1(10, [2, 8, 1, 9, 8, 5]));
+			assert.equal(2, s.solution1(11, [2, 8, 1, 9, 8, 5]));
+			assert.equal(4, s.solution1(13, [2, 8, 1, 9, 8, 5]));
+		});
+	});
+});
+
+describe('ex. 2', function() {
+	describe('#solution2()', function() {
+		it('no solutions', function() {
+			assert.equal(0, s.solution2(10, [1,1,1,1,1,1]));
+			assert.equal(0, s.solution2(3, [1,1,1,1,1,1]));
+			assert.equal(0, s.solution2(4, [4,4,4,4,4,4,5,6,7,8]));
+			assert.equal(0, s.solution2(4, [4,-1,4,1,4,-1,-4,1,4,1]));
+			assert.equal(0, s.solution2(1, [2,0,0,0,0,0,0,0,0,0]));
+			assert.equal(0, s.solution2(4, [1,0,0,1,0,0,1,0,1,0]));
+		});
+		it('right # of solutions', function() {
+			assert.equal(20, s.solution2(0, [4,-1,4,1,4,-1,-4,1,4,1]));
+			assert.equal(1, s.solution2(2, [1,0,0,0,0,0,0,0,0,0]));
+			assert.equal(2, s.solution2(10, [2,1,1,1,8,1]));
+			assert.equal(6, s.solution2(10, [2, 8, 1, 9, 8]));
+			assert.equal(8, s.solution2(10, [2, 3, 7, 9, 2, 1, 8]));
+			assert.equal(7, s.solution2(10, [2, 8, 1, 9, 8, 5]));
+			assert.equal(2, s.solution2(11, [2, 8, 1, 9, 8, 5]));
+			assert.equal(4, s.solution2(13, [2, 8, 1, 9, 8, 5]));
 		});
 	});
 });
