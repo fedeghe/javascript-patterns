4aa30652a347b491a72ff90c0bfe469b2a4879f0
fedeghe <fedeghe@gmail.com>
2021-01-05 00:43:04 +0100

    small thing

--- others/challenges/companies/hackerrank/2darrayDS.js
+++ others/challenges/companies/hackerrank/2darrayDS.js
@@ -1,16 +1,19 @@
 function hourglassSum(arr) {
     var max = -Infinity,
-        s = 6;
+        s = 6,
+        i, j;
     function checkMax(r, c){
       var rm = r - 1,
           rp = r + 1,
-          sum = arr[rm][c-1] + arr[rm][c] + arr[rm][c+1] +
+          cm = c - 1,
+          cp = c + 1,
+          sum = arr[rm][cm] + arr[rm][c] + arr[rm][cp] +
             arr[r][c] +
-            arr[rp][c-1] + arr[rp][c] + arr[rp][c+1];
+            arr[rp][cm] + arr[rp][c] + arr[rp][cp];
       if (max < sum) max = sum
     }
-    for (var i = 1; i < s-1; i++) {
-      for (var j = 1; j < s-1; j++) {
+    for (i = 0; i < s - 1; i++) {
+      for (j = 0; j < s - 1; j++) {
         checkMax(i, j)
       }
     }
